8a342035019c218f80478db0d58730ab40d954e9
bhandari_shubham <shubhambhandari12345hb@gmail.com>
2024-01-04 12:27:06 +0530

    mongo db connect

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import connectDB from "./db/index.js";
 import { app } from "./app.js";
 import { config } from "dotenv";
 config({
   path: "./.env",
 });
 
-app.listen(process.env.PORT ?? 8000, () => {
-  console.log(`Server is running at port:${process.env.PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log(`Server Error`, err);
+      throw err;
+    });
+    app.listen(process.env.PORT ?? 8000, () => {
+      console.log(`Server is running at port:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed !!!", err);
+  });
